f36ef41068c3577811461475e273b4d838717f4e
Jamila Aliyeva and Sharon Lee <sharon@>
2019-12-10 16:57:17 -0800

    add unfinished game to be continued

--- game.cs
+++ game.cs
@@ -1,42 +1,60 @@
 using System;
 using System.Collections.Generic;
 
- public class HigherLower 
- {
-     public int LowerNum;
-     public int HigherNum;
-     public int Guess;
+public class HighLow
+{
+    public int HighNum;
+    public int LowNum;
+    public int Guess;
+    public string Hint;
 
-    public static HigherLower()
+    public HighLow (int highNum, int lowNum)
     {
-        int LowerNum = lowernum;
-        int HigherNum = highernum;
-        int Guess = ((HigherNum + LowerNum) / 2);
+        HighNum = highNum;
+        LowNum = lowNum;
     }
+
+    public void Guessing()
+    {
+        Guess = (HighNum+LowNum)/2;
+        Console.WriteLine("Is your number higher or lower than "+Guess+"? (Higher/Lower/Correct)");
+        Hint = Console.ReadLine();
+        while (Hint != "Correct")
+        {
+            
+            if (Hint == "Higher")
+            {
+                LowNum = Guess;
+                Console.WriteLine("Lower number: "+ LowNum);
+            }
+            else if (Hint == "Lower")
+            {
+                HighNum = Guess;
+                Console.WriteLine("Higher number: "+ HighNum);
+            }
+            Guess = (HighNum+LowNum)/2;
+            Console.WriteLine("Is your number higher or lower than "+Guess+"? (Higher/Lower/Correct)");
+            Hint = Console.ReadLine();
+            
+            
+        }
+    }
+}
+
+public class Program
+{
     public static void Main()
     {
-        HigherLower instance = new HigherLower(1, 100);
-        Guess = ((HigherNum + LowerNum) / 2);
-        Console.WriteLine("Is you number higher or lower than", Guess);
-        string response = Console.ReadLine();
-        while (response != "correct") 
+        Console.WriteLine("Would you like to play the Higher/Lower game? (Yes/No)");
+        string choice = Console.ReadLine();
+        while (choice == "Yes")
         {
-            if (response == "higher")
-            {
-                LowerNum = Guess;
-            }
-            else if (response == "lower")
-            {
-                HigherNum = Guess; 
-            } 
-            else 
-            {
-                Console.WriteLine("Your number is", Guess);
-            }
-            Guess = ((HigherNum + LowerNum) / 2);
-            Console.WriteLine("Is you number higher or lower than", Guess);
+            HighLow gameTime = new HighLow(100,0);
+            Console.WriteLine("Okay");
+            gameTime.Guessing();
+            Console.WriteLine("Great! I guessed your number.  Would you like to play again? (Yes/No)");
+            choice = Console.ReadLine();
         }
 
-    }  
- }    
- 
+    }
+}
